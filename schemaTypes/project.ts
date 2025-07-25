673ff2d27c4dae571fba8866ed17f6195ad5b7a3
crendo11 <crendoc11@gmail.com>
2025-07-26 00:41:24 +0200

    sorting by set order

--- schemaTypes/project.ts
+++ schemaTypes/project.ts
@@ -6,6 +6,13 @@ export default defineType({
   type: 'document',
   fields: [
     defineField({
+      name: 'order',
+      title: 'Order',
+      type: 'number',
+      description: 'Lower numbers appear first',
+      initialValue: 0,
+    }),
+    defineField({
       name: 'title',
       title: 'Title',
       type: 'string',
